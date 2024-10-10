3f0728e4c30399e07224f8291baeda2b67fa7dcc
fractal void <kammel@posteo.de>
2024-10-10 21:39:05 +0200

    devolepment
    improved prototype element textbox,
    
    began improving element resizing
    
    unsure atm if resize adorners and textboxes are a good fit, because with tabs and enters, resizing is also possible, but clumsy, with adorners, it is flexible, but would need some more logic like scrollbars, or some way to resize on enters and tabs, which is blocked right now.
    
    before finishing this, the next goal is implementing a rudimentary xml based data serialization feature, to
    remove time costs when testing.

--- InnerComponents/ResizeAdorner.cs
+++ InnerComponents/ResizeAdorner.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using EBoard.Views;
+using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Media;
@@ -6,11 +7,26 @@ using System.Windows.Shapes;
 
 namespace EBoard.InnerComponents
 {
+    /// <summary>
+    /// 
+    /// integrate missing events for thumb3 and thumb4
+    /// 
+    /// rename thumbs to thumbCORNER or something alike
+    /// 
+    /// fix resize behaviour, which is kind of weird atm.
+    /// 
+    /// apply adorners to ElementView only, change width
+    /// and height properties of element via adorners
+    /// 
+    /// </summary>
     public class ResizeAdorner : Adorner
     {
         VisualCollection AdornerVisuals;
 
-        AdornerThumb thumb1, thumb2;
+        private ElementView _ElementView;
+
+
+        AdornerThumb thumb1, thumb2, thumb3, thumb4;
         Rectangle rectangle;
 
         double width = 10;
@@ -18,14 +34,18 @@ namespace EBoard.InnerComponents
 
         public ResizeAdorner(UIElement adornedElement) : base(adornedElement)
         {
+            _ElementView = (ElementView)adornedElement;
+
             AdornerVisuals = new VisualCollection(this);
             thumb1 = new AdornerThumb();
             thumb2 = new AdornerThumb();
+            thumb3 = new AdornerThumb();
+            thumb4 = new AdornerThumb();
 
             rectangle = new Rectangle() {
-                Stroke = Brushes.Coral,
-                StrokeThickness = 2,
-                StrokeDashArray = { 3, 2 }
+                Stroke = Brushes.DarkSlateGray,
+                StrokeThickness = 1,
+                StrokeDashArray = { 4, 4 }
             };
 
             thumb1.DragDelta += Thumb1_DragDelta;
@@ -34,21 +54,20 @@ namespace EBoard.InnerComponents
             AdornerVisuals.Add(rectangle);
             AdornerVisuals.Add(thumb1);
             AdornerVisuals.Add(thumb2);
-
+            AdornerVisuals.Add(thumb3);
+            AdornerVisuals.Add(thumb4);
         }
 
         private void Thumb1_DragDelta(object sender, DragDeltaEventArgs e)
-        {
-            var element = (FrameworkElement)AdornedElement;
-            element.Height = height - e.VerticalChange < 0 ? 0 : height - e.VerticalChange;
-            element.Width = width - e.HorizontalChange < 0 ? 0 : width - e.HorizontalChange;
+        {            
+            _ElementView.CurrentHeight = height - e.VerticalChange < 0 ? 0 : height - e.VerticalChange;
+            _ElementView.CurrentWidth = width - e.HorizontalChange < 0 ? 0 : width - e.HorizontalChange;
         }
 
         private void Thumb2_DragDelta(object sender, DragDeltaEventArgs e)
         {
-            var element = (FrameworkElement)AdornedElement;
-            element.Height = height + e.VerticalChange < 0 ? 0 : height + e.VerticalChange;
-            element.Width = width + e.HorizontalChange < 0 ? 0 : width + e.HorizontalChange;
+            _ElementView.CurrentHeight = height + e.VerticalChange < 0 ? 0 : height + e.VerticalChange;
+            _ElementView.CurrentWidth = width + e.HorizontalChange < 0 ? 0 : width + e.HorizontalChange;
         }
 
         protected override Visual GetVisualChild(int index)
@@ -63,7 +82,9 @@ namespace EBoard.InnerComponents
             rectangle.Arrange(new Rect(-2.5, -2.5, AdornedElement.DesiredSize.Width + 5, AdornedElement.DesiredSize.Height + 5));
 
             thumb1.Arrange(new Rect(-5,-5, width, height));
-            thumb2.Arrange(new Rect(AdornedElement.DesiredSize.Width - 5, AdornedElement.DesiredSize.Height - 5, width, height));
+            thumb2.Arrange(new Rect(AdornedElement.DesiredSize.Width - 5, - 5, width, height));
+            thumb3.Arrange(new Rect(AdornedElement.DesiredSize.Width - 5, AdornedElement.DesiredSize.Height - 5, width, height));
+            thumb4.Arrange(new Rect(- 5, AdornedElement.DesiredSize.Height - 5, width, height));
 
             return base.ArrangeOverride(finalSize);
         }
